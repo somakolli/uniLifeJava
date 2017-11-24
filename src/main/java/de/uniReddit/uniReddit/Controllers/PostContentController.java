724e1ddbd4e62d947fee912716f1e0cf38a47fc9
Sokol Makolli <p4ch1n0@aol.de>
2017-11-24 11:32:11 +0100

    Added PostContentController.

---
+++ src/main/java/de/uniReddit/uniReddit/Controllers/PostContentController.java
@@ -0,0 +1,25 @@
+package de.uniReddit.uniReddit.Controllers;
+
+import de.uniReddit.uniReddit.Models.PostContent;
+import de.uniReddit.uniReddit.Repositories.PostContentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/postContent")
+public class PostContentController {
+    private final PostContentRepository postContentRepository;
+    @Autowired
+    PostContentController(PostContentRepository postContentRepository){
+        this.postContentRepository = postContentRepository;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{postContentId)")
+    PostContent get(@PathVariable Long postContentId){
+        return postContentRepository.findOne(postContentId);
+    }
+}
