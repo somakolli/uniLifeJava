06bdd4f43c7ded40c727b79ad49c3817e72d7cba
p4ch1n0 <p4ch1n0@aol.de>
2017-11-27 21:41:47 +0100

    added gcloud config

--- src/main/java/de/uniReddit/uniReddit/UniRedditApplication.java
+++ src/main/java/de/uniReddit/uniReddit/UniRedditApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class UniRedditApplication {
@@ -18,5 +19,11 @@ public class UniRedditApplication {
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@RequestMapping("/_ah/health")
+	public String healthy() {
+		// Message body required though ignored
+		return "Still surviving.";
 	}
 }
