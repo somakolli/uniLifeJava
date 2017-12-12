c6da69cbfc82637d4f17486d222c050f647229c2
Sokol <p4ch1n0@aol.de>
2017-12-12 14:37:44 +0100

    added change db script

---
+++ changeDB.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+mv src/main/resources/application.properties src/main/resources/application.properties.h2
+mv src/main/resources/application.properties.post src/main/resources/application.properties
