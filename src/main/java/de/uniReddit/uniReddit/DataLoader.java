bd741ef04e7e802154252b46a1cf240ba76aeda5
Sokol Makolli <st116214@stud.uni-stuttgart.de>
2019-02-14 21:07:26 +0100

    change number of dummies

--- src/main/java/de/uniReddit/uniReddit/DataLoader.java
+++ src/main/java/de/uniReddit/uniReddit/DataLoader.java
@@ -57,14 +57,14 @@ public class DataLoader implements ApplicationRunner {
             userRepository.save(sokol);
 
             List<UtUser> users =  createDummyUsers(5, university);
-            List<UniSubject> subjects = createDummySubjects(100, university);
+            List<UniSubject> subjects = createDummySubjects(10, university);
             for (UniSubject subject: subjects) {
                 sokol.subscribe(subject);
                 uniSubjectRepository.save(subject);
             }
             userRepository.save(sokol);
-            List<UniThread> threads = createDummyThreads(1000, users, subjects);
-            List<Comment> comments = createDummyComments(1000, users, threads);
+            List<UniThread> threads = createDummyThreads(100, users, subjects);
+            List<Comment> comments = createDummyComments(100, users, threads);
             System.out.println("Dummy data created.");
         }
     }
